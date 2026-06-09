66168d230e8dcaa115a10f38d73ed437502824aa
kakaomames <kakaomames@example.com>
2026-06-09 17:52:47 +0900

    UPDATE: Current proxy URL to https://982816cb0b2622.lhr.life

---
+++ syp.py
@@ -0,0 +1,14 @@
+import subprocess
+import time
+
+def main():
+    print("[LOG] syp.sh 実行")
+    subprocess.run(["bash", "syp.sh"])
+
+if __name__ == '__main__':
+    print("[LOG] SYSTEM: ========================================================")
+    print("[LOG] SYSTEM: ========================================================")
+
+    while True:
+        main()
+        time.sleep(3600)  # 1時間待機
